ba25fd54003a80034b96af0101ef707b082c47c7
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-05-14 17:02:14 -0300

    tela preta e corrigindo a reta tracejada

--- radar/library/library.py
+++ radar/library/library.py
@@ -139,12 +139,12 @@ def Reta(x1,y1,x2,y2, cor, primitiva, janela, estilo = 0):
                 
                 elif estilo == 2:   
                     # Reta Tracejada 
-                    if counter == 0 or counter == 1 or counter == 2 or counter == 3 or counter == 4:
+                    if counter == 2 or counter == 3 or counter == 4 or counter == 5:
                         counter += 1
                         Ponto(x, y, cor, primitiva, janela, False)
                     else:
                         counter += 1
-                        if counter == 9:
+                        if counter == 10:
                             counter = 0
 
                 else:
@@ -194,12 +194,12 @@ def Reta(x1,y1,x2,y2, cor, primitiva, janela, estilo = 0):
                 
                 elif estilo == 2:   
                     # Reta Tracejada 
-                    if counter == 0 or counter == 1 or counter == 2 or counter == 3:
+                    if counter == 2 or counter == 3 or counter == 4 or counter == 5:
                         counter += 1
                         Ponto(x, y, cor, primitiva, janela, False)
                     else:
                         counter += 1
-                        if counter == 9:
+                        if counter == 10:
                             counter = 0
 
                 else:
